c26c87c60b2f098ce47de99f79a50e3da15dc809
hyujisf <canqalloid.me@gmail.com>
2022-07-18 11:01:34 +0700

    update nvim lualine UI

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
-  alias ll "exa -l -g --icons  "
+if type -q exa
+  alias ll "exa -l -g --icons"
   alias lla "ll -a"
-
+end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -4,9 +4,11 @@ if (not status) then return end
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'solarized_dark',
-    section_separators = {left = '', right = ''},
-    component_separators = {left = '', right = ''},
+    theme = 'nightfly',
+    section_separators = {left = '', right = ''},
+    component_separators = {left = '', right = ''},
+    -- section_separators = {left = '', right = ''},
+    -- component_separators = {left = '', right = ''},
     disabled_filetypes = {}
   },
   sections = {
